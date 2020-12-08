3e1f367fdbb6f7c6f574e35d312e91b1095645f0
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-12-07 23:27:11 -0300

    Configured .env file

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
-const PORT = 3000;
 const app = express();
+
+const PORT = process.env.DEFAULT_PORT;
 
 app.get('/', (req, res) => {
     res.status(200).json({message: 'Four Against Darkness'});

e79f2f85c2690c0e891eac0ae167d733b7dcaaa5
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-12-07 23:41:04 -0300

    Added routes layer

--- src/app.js
+++ src/app.js
@@ -2,14 +2,14 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 require('dotenv').config();
+// routes
+const mainRoutes = require('./routes/main');
 
 const app = express();
 
 const PORT = process.env.DEFAULT_PORT;
 
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'Four Against Darkness'});
-});
+app.use('/api', mainRoutes);
 
 app.listen(PORT, () => {
     console.log(`Application is running on port ${PORT}`);

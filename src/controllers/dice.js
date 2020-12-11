6a7e0a6d4ef655cdf7fe67adcf8b7d33c0ef7558
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-12-11 00:02:09 -0300

    Added validation on dice endpoint

--- src/controllers/dice.js
+++ src/controllers/dice.js
@@ -1,28 +1,43 @@
 exports.rollDices = (req, res) => {
-    var diceList = [];
-    var hist = '';
-    var sum = 0;
-    var dices = []
+	var diceList = [];
+	var hist = '';
+	var sum = 0;
+	var dices = []
 
-    diceList = req.body.dices;
-    console.log(diceList);
+	diceList = req.body.dices;
 
-    diceList.forEach((dice) => {
-        for(let i = 0; i < dice.qty; i++) {
-            let roll = Math.floor(Math.random() * dice.faces) + 1;
-            
-            sum += roll;
-            hist += `${roll} (d${dice.faces}) + `;
-            dices.push({
-                faces: dice.faces,
-                value: roll
-            });
-        }        
-    });
+	if(diceList == undefined || diceList.length == 0) {
+		res.status(400).json({message: `No dice(s) informed`});
+	}
 
-    let opsAux = hist.split('');
-    opsAux.splice(opsAux.length - 3, 3);
-    hist =  opsAux.join('') + ` = ${sum}`;
+	diceList.forEach((dice) => {
+		if(this.isDiceValid(dice)) {
+			for(let i = 0; i < dice.qty; i++) {
+				let roll = Math.floor(Math.random() * dice.faces) + 1;
+				
+				sum += roll;
+				hist += `${roll} (d${dice.faces}) + `;
+				dices.push({
+					faces: dice.faces,
+					value: roll
+				});
+			}        
+		} else {
+			res.status(400).json({message: 'Invalid dice type', dice});
+		}
+	});
 
-    res.status(200).json({sum, hist, dices});
+	let opsAux = hist.split('');
+	opsAux.splice(opsAux.length - 3, 3);
+	hist =  opsAux.join('') + ` = ${sum}`;
+
+	res.status(200).json({sum, hist, dices});
+}
+
+exports.isDiceValid = (dice) => {
+	if(dice.faces == undefined || dice.faces == 0) {
+		return false;
+	}
+
+	return true;
 }

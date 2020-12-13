618f1b9017ab2eecbdd0a7b8465cb1efae1854ec
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-12-13 12:05:29 -0300

    Connected to database (MongoDB Atlas)

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 require('dotenv').config();
 // routes
 const mainRoutes = require('./routes/main');
@@ -9,6 +10,7 @@ const diceRoutes = require('./routes/dice');
 const app = express();
 
 const PORT = process.env.DEFAULT_PORT;
+const DB_CONNECTION = process.env.DB_CONN_STRING;
 
 // middleware
 app.use(morgan('dev'));
@@ -17,6 +19,18 @@ app.use(bodyParser.json());
 // routing
 app.use('/api', mainRoutes);
 app.use('/api', diceRoutes);
+
+// DB connection
+mongoose.connect(DB_CONNECTION, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('DB connected');
+}).catch((err) => {
+    console.log(`Error connecting to the database:\n ${err}`)
+});
 
 app.listen(PORT, () => {
     console.log(`Application is running on port ${PORT}`);
